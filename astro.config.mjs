ff6e41e450e5f8f742f7fa90ad4b1475bcd32790
Anthony Tepach <anthony.tepach@gmail.com>
2023-01-17 17:40:00 -0600

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://anthonytepach.github.io',
+  base: '/my-repo',
   integrations: [tailwind(), sitemap()]
 });
